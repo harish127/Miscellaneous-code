231335b2913bae8d9b839bf2f9a09e201641b6b0
harish <harishkaiwart127@gmail.com>
2021-09-27 10:31:38 +0530

    second problem done it time 30 min to do all thing together including pushing and surfing. litening to music

---
+++ lovebaber/max_min_arr.cpp
@@ -0,0 +1,61 @@
+/*
+
+Maximum and minimum of an array using minimum number of comparisons
+
+Coded by: Harish Kumar
+Date: 27/09/2021
+
+INPUT
+N
+a1 a2 ... an
+
+OUTPUT
+Z X
+
+where Z is maximum and X is minimum element in array.
+
+*/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//function for returning minimum and maximum element in array
+pair<int , int> max_min(vector<int> &v){
+    
+    if(v.size()==1){
+        return make_pair(v[0],v[0]);
+    }
+    int maxElem=-9999, minElem=9999;
+    //O(n) approach
+    for(int i=0;i<v.size();i++){
+        if(v[i]>maxElem) maxElem=v[i];
+        if(v[i]<minElem) minElem=v[i];
+    }
+
+    // //O(logn) approach
+    // sort(v.begin(),v.end());
+    // return make_pair(v[0],v[v.size()-1]);
+
+    return make_pair(maxElem,minElem);
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n,0);
+
+    //Inputing the Values to array
+    for(int i=0;i<v.size();i++)
+        cin>>v[i];
+    
+    //solution goes here
+    pair<int , int> sol = max_min(v);
+    
+    // MAX then MIN
+    cout<<sol.first<<" "<<sol.second;
+    
+}
