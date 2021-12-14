ba7d78cb2cfa027f818f54c39a059097710a0e6f
Harish Kumar <harishkaiwart127@gmail.com>
2021-12-14 11:30:06 +0530

    tested working

--- lovebaber/min_jmp.cpp
+++ lovebaber/min_jmp.cpp
@@ -2,8 +2,7 @@
 
 Minimum number of jumps
 
-Done code not tested on platform
-
+My code not pass testcase so i saw solution but it shows TLE finally i saw good solution here it is 
 Coded By: Harish Kumar
 Date: 11/12/2021
 
@@ -16,16 +15,36 @@ Output: 3
 
 #include<iostream>
 #include<vector>
-
+#include<algorithm>
 using namespace std;
 
 int minJumps(int n,vector<int> arr){
-    int noOfJumps=0;
-    for(int i=arr[0];i<=n;i+=arr[i]){
-        noOfJumps++;
-        if(arr[i]==0) return -1;
-    }
-    return noOfJumps;
+ 
+ if(n <= 1) 
+ return 0;
+ if(arr[0] == 0) 
+ return -1;
+
+ int maxreach = arr[0];
+
+ int step = arr[0];
+
+ int jump =1;
+ int i=1;
+ for(i=1;i<n;i++){
+     if(i==n-1)
+     return jump;
+     maxreach = max(maxreach,i+arr[i]);
+     step--;
+     if(step ==0){
+         jump ++;
+         if(i>=maxreach)
+         return -1;
+
+         step=maxreach-i;;
+     }
+ }
+ return -1;
 }
 
 int main(){
